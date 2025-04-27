4df603b0bbcc46cc586891a14cdca776ff07f3f8
Ikpambese <ikpambeseb@gmail.com>
2025-04-27 20:29:22 +0100

    pushed version of android

--- lib/payment/flutterwave_payment.dart
+++ lib/payment/flutterwave_payment.dart
@@ -3,63 +3,6 @@ import 'package:flutterwave_standard/flutterwave.dart';
 import 'package:hiiveuser/assistants/key.dart';
 import 'package:uuid/uuid.dart';
 
-// class OyaPay {
-//   OyaPay(
-//       {required this.ctx,
-//       required this.price,
-//       required this.email,
-//       required this.logistics});
-//   final BuildContext ctx;
-//   final int? price;
-//   final String email;
-//   final int logistics;
-
-//   bool isTestMode = false;
-
-//   Future<void> handlePaymentInitialization() async {
-//     final Customer customer = Customer(email: email);
-//     final realPrice = price! + logistics;
-//     print(realPrice);
-//     final Flutterwave flutterwave = Flutterwave(
-//       context: ctx,
-//       publicKey: ConstantKey.PAYSTACK_KEY,
-//       currency: 'NGN',
-//       redirectUrl: 'https://www.google.com',
-//       txRef: const Uuid().v1(),
-//       amount: realPrice.toString(),
-//       customer: customer,
-//       paymentOptions: "card, payattitude, barter, bank transfer, ussd",
-//       customization: Customization(title: "Hiive Payment"),
-//       isTestMode: isTestMode,
-//     );
-//     final ChargeResponse response = await flutterwave.charge();
-// //final ChargeResponse response = await flutterwave.charge();
-
-//     showLoading(response.toString());
-//     print("${response.toJson()}");
-//   }
-
-//   // String getPublicKey() {
-//   //   return ConstantKey.PAYSTACK_KEY;
-//   // }
-
-//   Future<void> showLoading(String message) {
-//     return showDialog(
-//       context: ctx,
-//       barrierDismissible: true,
-//       builder: (BuildContext context) {
-//         return AlertDialog(
-//           content: Container(
-//             margin: EdgeInsets.fromLTRB(30, 20, 30, 20),
-//             width: double.infinity,
-//             height: 50,
-//             child: Text(message),
-//           ),
-//         );
-//       },
-//     );
-//   }
-// }
 class OyaPay {
   OyaPay(
       {required this.ctx,
@@ -79,7 +22,8 @@ class OyaPay {
     final realPrice = price! + logistics;
     print(realPrice);
     final Flutterwave flutterwave = Flutterwave(
-      context: ctx,
+      //ctx: context,
+      // Fixed: using correct parameter name 'ctx'
       publicKey: ConstantKey.PAYSTACK_KEY,
       currency: 'NGN',
       redirectUrl: 'https://www.google.com',
@@ -90,7 +34,7 @@ class OyaPay {
       customization: Customization(title: "Hiive Payment"),
       isTestMode: isTestMode,
     );
-    final ChargeResponse response = await flutterwave.charge();
+    final ChargeResponse response = await flutterwave.charge(ctx);
 
     if (response.success!) {
       showLoading("Payment successful");
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -13,6 +13,7 @@ import firebase_auth
 import firebase_core
 import firebase_messaging
 import firebase_storage
+import flutter_inappwebview_macos
 import flutter_local_notifications
 import geolocator_apple
 import path_provider_foundation
@@ -29,6 +30,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
